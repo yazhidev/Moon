bde861b11fa0892d2411184cba2960251cc6e86d
zengyazhi <yazhi1992@163.com>
2017-12-23 15:29:50 +0800

    commit

---
+++ app/src/main/java/com/yazhi1992/moon/Main2Activity.kt
@@ -0,0 +1,15 @@
+package com.yazhi1992.moon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        floatingBtn.setOnClickListener {  }
+
+    }
+}
