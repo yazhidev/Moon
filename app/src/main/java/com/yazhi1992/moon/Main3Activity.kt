49847709fe6df6712b7442f66d42ffab9b619534
zengyazhi <yazhi1992@163.com>
2017-12-29 23:20:25 +0800

    透明状态栏（水波效果延伸至状态栏）

--- app/src/main/java/com/yazhi1992/moon/Main3Activity.kt
+++ app/src/main/java/com/yazhi1992/moon/Main3Activity.kt
@@ -16,7 +16,6 @@ class Main3Activity : AppCompatActivity() {
         setContentView(R.layout.activity_main3)
 
         StatusBarUtils.with(this)
-                .setColor(resources.getColor(R.color.colorAccent))
                 .init()
 
         findViewById<View>(R.id.button).setOnClickListener { val main3Activity = this@Main3Activity }
@@ -24,10 +23,10 @@ class Main3Activity : AppCompatActivity() {
         outview.post(object : Runnable {
             override fun run() {
                 outview.visibility = View.VISIBLE
-            val centerX = 500
-            val centerY = 1000
-            val createCircularReveal = ViewAnimationUtils.createCircularReveal(outview, centerX, centerY, 0F, Math.sqrt(Math.pow(centerX.toDouble(), 2.0) + Math.pow(centerY.toDouble(), 2.0)).toFloat())
-            createCircularReveal.setDuration(1000).start()
+                val centerX = fab.left + fab.width / 2
+                val centerY = fab.top + fab.width / 2
+                val createCircularReveal = ViewAnimationUtils.createCircularReveal(outview, centerX, centerY, 0F, Math.sqrt(Math.pow(centerX.toDouble(), 2.0) + Math.pow(centerY.toDouble(), 2.0)).toFloat())
+                createCircularReveal.setDuration(1000).start()
             }
         })
     }
